a7f5c6644018009a150dd4a452c8049507e6b5ba
Ajhere95 <162614584+Ajhere95@users.noreply.github.com>
2024-07-20 19:35:25 +0530

    Add files via upload

---
+++ complex.py
@@ -0,0 +1,11 @@
+import math
+a = int(input('Enter a : '))
+b = int(input('Enter b : '))
+x = int(input('Enter x : '))
+y = int(input('Enter y : '))
+
+m = complex(a,b)
+n= complex(x,y)
+
+print(m+n)
+print(m*n)
